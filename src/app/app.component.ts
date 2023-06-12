5a4a039e6e7d369a3b92be4a3b45ad00cf497ade
shubh1007 <b200045@nitsikkim.ac.in>
2023-06-12 16:00:45 +0530

    Interface Completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,17 +2,34 @@ import { Component, Output, EventEmitter } from '@angular/core';
 import { UserdataService } from './userdata.service';
 import { UserAddressService } from './user-address.service';
 import { Observable } from 'rxjs';
+import { Employee } from './empInterface';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
 export class AppComponent {
-  userData$:Observable<any>;
-  constructor(
-    private userDataService: UserdataService
-  ){
-    this.userData$ = this.userDataService.getUserData();
+
+  constructor(){
+
   }
+  getData(){
+    let companyEmp: Employee = {
+      // Here no validation of the variables are done till now.
+      // In email we can add number, string or any other data type.
+      // Similarly in phone we can add string, number or any other data type.
+      // name: 1234, throws an error.
+      name: "Shubham",
+      age: 22,
+      email: "shubhsingtam10@gmail.com",
+      phone: 9999999999
+      // It also throws an error if we don't add the phone variable. To avoid this we can add ? after the variable name.
+      // phone: 7098213317
+    }
+    return companyEmp;
+  }
+
 }
---
+++ src/app/empInterface.ts
@@ -0,0 +1,6 @@
+export interface Employee{
+    name: string;
+    age: number;
+    email: any;
+    phone?: number;
+}
